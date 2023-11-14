c89c7e9ac4ad5cce766fac5040e8b8e991a79a92
Adiyan Mustahim <50477931+dhdeinfo@users.noreply.github.com>
2023-11-14 18:55:49 +0700

    Create ads.js
    add

---
+++ ads.js
@@ -0,0 +1,10 @@
+// your-ad-script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var adContainer = document.getElementById("adContainer");
+
+    // Gantilah dengan konten iklan yang sesuai dengan kebutuhan Anda
+    var adContent = '<a href="https://example.com"><img src="https://example.com/your-ad-image.jpg" alt="Advertisement"></a>';
+
+    // Sisipkan konten iklan ke dalam elemen target
+    adContainer.innerHTML = adContent;
+});
